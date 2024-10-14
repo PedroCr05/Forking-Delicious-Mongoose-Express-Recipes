c35e0efa8e0fe40e63c28388b86859c9c3db8050
pedrocruz <JayJMarquez05@gmail.com>
2024-10-13 21:15:02 -0500

    Directions has now been built

--- models/Direction.js
+++ models/Direction.js
@@ -1,5 +1,17 @@
 const { Schema } = require(`mongoose`);
 
-const DirectionSchema = new Schema({});
+const DirectionSchema = new Schema(
+  {
+    recipe: { type: Schema.Types.ObjectId, ref: `Recipes` },
+    directions: [
+      {
+        stepNumber: { type: Number, required: true },
+        instructions: { type: String, required: true },
+        temperature: { type: Number },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = DirectionSchema;
